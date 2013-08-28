25e80cee16cd5af505197d0a8d6d343f820e1b2d
annakryzhko1 <anna.kryzhko@gmail.com>
2013-08-28 20:49:58 +0300

    format fix

--- App1/app/controller/Users.js
+++ App1/app/controller/Users.js
@@ -1,93 +1,77 @@
 Ext.define('AM.controller.Users', {
-    extend: 'Ext.app.Controller',
-	
-	stores: [
-        'Users'
-    ],
-    
-    models: [
-    	'User'
-    ],
-    
-    views: [
-        'user.List',
-        'user.Edit'
-    ],
-    
-    init: function() {
-    	this.sorters = { 
-    		byDomain: new Ext.util.Sorter({
-        		sorterFn: function(o1, o2) {
-	            var domain1 = o1.get('email').split('@')[1],
-	            	domain2 = o2.get('email').split('@')[1]
-	            if (domain1 === domain2) {
-	                return 0;
-	            }	
-	            return domain1 < domain2 ? -1 : 1;
-        	}
-    	})
-    	};
-    	
-        this.control({
-            'userlist': {
-                itemdblclick: this.editUser
-            },            
-            'useredit button[action=save]': {
-                click: this.updateUser
-            },            
-            'useredit button[action=delete]': {
-                click: this.deleteUser
-            },
-            'userlist textfield[name=nameFilter]': {
-                change: this.filterByName
-            },
-            'userlist button[action=sortByDomain]': {
-                click: this.sortByDomain
-            }
-        });
-    },
+	extend : 'Ext.app.Controller',
 
-    editUser: function(grid, record) {
-         var view = Ext.widget('useredit');
+	stores : ['Users'],
 
-        view.down('form').loadRecord(record);
-    },
-    
-    updateUser: function(button) {
-	    var win    = button.up('window'),
-	        form   = win.down('form'),
-	        record = form.getRecord(),
-	        values = form.getValues();
-	
-	    record.set(values);
-	    win.close();
-	    this.getUsersStore().sync();
+	models : ['User'],
+
+	views : ['user.List', 'user.Edit'],
+
+	init : function() {
+		this.sorters = {
+			byDomain : new Ext.util.Sorter({
+				sorterFn : function(o1, o2) {
+					var domain1 = o1.get('email').split('@')[1], domain2 = o2.get('email').split('@')[1]
+					if (domain1 === domain2) {
+						return 0;
+					}
+					return domain1 < domain2 ? -1 : 1;
+				}
+			})
+		};
+
+		this.control({
+			'userlist' : {
+				itemdblclick : this.editUser
+			},
+			'useredit button[action=save]' : {
+				click : this.updateUser
+			},
+			'useredit button[action=delete]' : {
+				click : this.deleteUser
+			},
+			'userlist textfield[name=nameFilter]' : {
+				change : this.filterByName
+			},
+			'userlist button[action=sortByDomain]' : {
+				click : this.sortByDomain
+			}
+		});
 	},
-	
-	
-    deleteUser: function(button) {
-	    var win    = button.up('window'),
-	        form   = win.down('form'),
-	        record = form.getRecord();
-	
-	    this.getUsersStore(record);
-	    
-	    win.close();
-	    
-	    this.getUsersStore().sync();
+
+	editUser : function(grid, record) {
+		var view = Ext.widget('useredit');
+
+		view.down('form').loadRecord(record);
 	},
-	
-	filterByName: function(textfield, newValue, oldValue, eOpts ) {
+
+	updateUser : function(button) {
+		var win = button.up('window'), form = win.down('form'), record = form.getRecord(), values = form.getValues();
+
+		record.set(values);
+		win.close();
+		this.getUsersStore().sync();
+	},
+
+	deleteUser : function(button) {
+		var win = button.up('window'), form = win.down('form'), record = form.getRecord();
+
+		this.getUsersStore(record);
+
+		win.close();
+
+		this.getUsersStore().sync();
+	},
+
+	filterByName : function(textfield, newValue, oldValue, eOpts) {
 		var store = this.getUsersStore();
 		store.clearFilter(true);
-		store.filter("name", new RegExp(newValue, "i"));	
+		store.filter("name", new RegExp(newValue, "i"));
 	},
-	
-	sortByDomain: function(button, e, eOpts) {
-		var sorter = this.sorters['byDomain'],
-		 	dir = sorter.direction; 
-		sorter.setDirection((dir=='ASC') ? 'DESC': 'ASC')
-		this.getUsersStore().sort(sorter);		
+
+	sortByDomain : function(button, e, eOpts) {
+		var sorter = this.sorters['byDomain'], dir = sorter.direction;
+		sorter.setDirection((dir == 'ASC') ? 'DESC' : 'ASC')
+		this.getUsersStore().sort(sorter);
 	}
-    
-});
+}); 
--- App1/app/view/user/List.js
+++ App1/app/view/user/List.js
@@ -1,23 +1,34 @@
-Ext.define('AM.view.user.List' ,{
-    extend: 'Ext.grid.Panel',
-    alias: 'widget.userlist',
-	store: 'Users',
-    title: 'All Users',
-	dockedItems: [{
-    xtype: 'toolbar',
-    dock: 'top',
-    	items: [
-    	    { xtype: 'textfield', name: 'nameFilter', fieldLabel: 'Filter by name'},
-        	{ xtype: 'button', text: 'Sort by domen', action: 'sortByDomain' }
-    	]
+Ext.define('AM.view.user.List', {
+	extend : 'Ext.grid.Panel',
+	alias : 'widget.userlist',
+	store : 'Users',
+	title : 'All Users',
+	dockedItems : [{
+		xtype : 'toolbar',
+		dock : 'top',
+		items : [{
+			xtype : 'textfield',
+			name : 'nameFilter',
+			fieldLabel : 'Filter by name'
+		}, {
+			xtype : 'button',
+			text : 'Sort by domen',
+			action : 'sortByDomain'
+		}]
 	}],
-    initComponent: function() {
-        
-        this.columns = [
-            {header: 'Name',  dataIndex: 'name',  flex: 1},
-            {header: 'Email', dataIndex: 'email', flex: 1}
-        ];
+	
+	initComponent : function() {
 
-        this.callParent(arguments);
-    }
-});
+		this.columns = [{
+			header : 'Name',
+			dataIndex : 'name',
+			flex : 1
+		}, {
+			header : 'Email',
+			dataIndex : 'email',
+			flex : 1
+		}];
+
+		this.callParent(arguments);
+	}
+}); 
